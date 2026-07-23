40365d7384be3c5f3045b5586656f4611e821c2a
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-23 19:14:47 +0530

    Completed Problem 6

--- Problem - 5 (Hard).py
+++ Problem - 5 (Hard).py
@@ -64,3 +64,49 @@
 # 36000
 # 70000
 # 53000
+
+class Employee:
+    def __init__(self, name: str, salary: float):
+        self.__name=name
+        self.__salary=salary
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def salary(self):
+        return self.__salary
+
+    def yearly_salary(self):
+        return self.__salary*12
+
+    def __str__(self):
+        return f"{self.__name}, salary {self.__salary}"
+
+class Manager(Employee):
+    def __init__(self, name, salary, bonus):
+        super().__init__(name, salary)
+        self.__bonus=bonus
+
+    def yearly_salary(self):
+        return super().yearly_salary()+self.__bonus
+
+class Developer(Employee):
+    def __init__(self, name, salary, proj_comp):
+        super().__init__(name, salary)
+        self.__proj_comp=proj_comp
+
+    def yearly_salary(self):
+        return super().yearly_salary()+(self.__proj_comp*1000)
+
+def main():
+    e = Employee("Peter", 3000)
+    m = Manager("Maria", 5000, 10000)
+    d = Developer("John", 4000, 5)
+
+    print(e.yearly_salary())
+    print(m.yearly_salary())
+    print(d.yearly_salary())
+if __name__=="__main__":
+    main()

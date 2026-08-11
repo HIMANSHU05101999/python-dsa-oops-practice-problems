df8ebb228ab9348c9f1668ceda6b4e3593cd6a10
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-08-11 17:32:22 +0530

    Completed Problem - 40

--- Problem - 40.py
+++ Problem - 40.py
@@ -33,3 +33,69 @@
 # Do NOT use max().
 #
 # WRITE YOUR SOLUTION BELOW:
+
+# ============================================================
+# RECURSION + DSA EXERCISE 16 — Sum Tree Values
+# ============================================================
+#
+# Write:
+#
+# tree_sum(root)
+#
+# It should return the sum of all values in the tree.
+#
+# Example:
+#
+#             10
+#            /  \
+#           5    15
+#          / \
+#         2   7
+#
+# Result:
+#
+# 10 + 5 + 15 + 2 + 7 = 39
+#
+# tree_sum(root)
+# -> 39
+#
+# Hints:
+#
+# Empty tree -> 0
+#
+# Otherwise:
+#
+# root.value
+# + sum of left subtree
+# + sum of right subtree
+#
+# WRITE YOUR SOLUTION BELOW:
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value=value
+        self.left=left
+        self.right=right
+
+def max_node(node: Node):
+    if node==None:
+        return
+
+    max=node.value
+    if node.left:
+        if max<max_node(node.left):
+            max=max_node(node.left)
+
+    if node.right:
+            if max<max_node(node.right):
+                max=max_node(node.right)
+    return max
+
+if __name__=="__main__":
+    root=Node(10)
+    root.left=Node(5)
+    root.left.left=Node(2)
+    root.left.right=Node(7)
+    root.right=Node(15)
+
+    print(max_node(root))

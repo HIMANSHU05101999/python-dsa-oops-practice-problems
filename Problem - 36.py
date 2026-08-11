4086ddf098f418823917e913edc10ca8b7216259
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-08-11 10:13:34 +0530

    Completed Problem - 36

--- Problem - 36.py
+++ Problem - 36.py
@@ -30,3 +30,29 @@
 #     recursively search node.next
 #
 # WRITE YOUR SOLUTION BELOW:
+
+class Node:
+    def __init__(self, val, next=None):
+        self.val=val
+        self.next=next
+
+def find_node(node, target):
+    if node == None:
+        return False
+
+    if node.val==target:
+        return True
+    
+    return find_node(node.next,target)
+    
+    
+
+if __name__=="__main__":
+    node1 = Node(10)
+    node2 = Node(20)
+    node3 = Node(30)
+
+    node1.next = node2
+    node2.next = node3
+
+    print(find_node(node1,60))
